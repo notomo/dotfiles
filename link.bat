33be884aaf6938eee489f047b2ea7c6d0ecb8ffa
tmn-o3 <tomorrow2009@live.jp>
2016-04-18 22:12:08 +0900

    link.bat modified

--- link.bat
+++ link.bat
@@ -1,3 +1,4 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink /D %HOMEPATH%"\.vim\userautoload" %HOMEPATH%"\dotfiles\userautoload"
 pause
