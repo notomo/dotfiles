323561041445f24d163cfb9ac65d1911b5c69cd5
tmn-o3 <tomorrow2009@live.jp>
2017-03-26 11:23:08 +0900

    add tool, fix and clean up .gitignore, add neovim link settings.

--- link.bat
+++ link.bat
@@ -1,7 +1,13 @@
-mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\vim\rc\.vimrc"
-mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\vim\rc\.gvimrc"
-mklink %HOMEPATH%"\.vimrc_first.vim" %HOMEPATH%"\dotfiles\vim\rc\kaoriya\.vimrc_first.vim"
-mklink %HOMEPATH%"\.gvimrc_first.vim" %HOMEPATH%"\dotfiles\vim\rc\kaoriya\.gvimrc_first.vim"
+
+cd %HOMEPATH%
+mklink ".vimrc" %HOMEPATH%"\dotfiles\vim\rc\.vimrc"
+mklink ".gvimrc" %HOMEPATH%"\dotfiles\vim\rc\.gvimrc"
+mklink ".vimrc_first.vim" %HOMEPATH%"\dotfiles\vim\rc\kaoriya\.vimrc_first.vim"
+mklink ".gvimrc_first.vim" %HOMEPATH%"\dotfiles\vim\rc\kaoriya\.gvimrc_first.vim"
+
+mkdir %HOMEPATH%"\.vim" > NUL 2>&1
+if ERRORLEVEL 1 cmd /c exit 0
+
 mklink /D %HOMEPATH%"\.vim\rc" %HOMEPATH%"\dotfiles\vim\rc"
 mklink /D %HOMEPATH%"\.vim\autoload" %HOMEPATH%"\dotfiles\vim\autoload"
 mklink /D %HOMEPATH%"\.vim\dict" %HOMEPATH%"\dotfiles\vim\dict"
@@ -9,5 +15,13 @@ mklink /D %HOMEPATH%"\.vim\snippets" %HOMEPATH%"\dotfiles\vim\snippets"
 mklink /D %HOMEPATH%"\.vim\tmp" %HOMEPATH%"\dotfiles\vim\tmp"
 mklink /D %HOMEPATH%"\.vim\after" %HOMEPATH%"\dotfiles\vim\after"
 
-mklink %HOMEPATH%"\.ideavimrc" %HOMEPATH%"\dotfiles\idea\.ideavimrc"
+mkdir %HOMEPATH%"\.config" > NUL 2>&1
+if ERRORLEVEL 1 cmd /c exit 0
+
+mklink /D %HOMEPATH%"\.config\nvim" "\.vim"
+cd %HOMEPATH%"\.config\nvim"
+mklink "init.vim" %HOMEPATH%"\dotfiles\vim\rc\init.vim"
+
+cd %HOMEPATH%
+mklink ".ideavimrc" %HOMEPATH%"\dotfiles\idea\.ideavimrc"
 pause
---
+++ vim/tool/gvim_startup_log.bat
@@ -0,0 +1,7 @@
+SET log_file=../tmp/startup_log.log
+echo; >> %log_file%
+echo; >> %log_file%
+echo ##################### >> %log_file%
+echo %date% %time% >> %log_file%
+echo ##################### >> %log_file%
+start gvim --startuptime %log_file%
