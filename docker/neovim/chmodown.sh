7e3a7f60b622edd04cfa635c59951454fffc406d
tmn-o3 <tomorrow2009@live.jp>
2017-05-29 22:37:21 +0900

    fix dockerfile

--- docker/neovim/chmodown.sh
+++ docker/neovim/chmodown.sh
@@ -1,6 +1,7 @@
 
 USERNAME=user
 USERDIR=/home/$USERNAME
+VAGRANTKEY=$USERDIR/.ssh/vagrant_private_key
 
 mkdir -p $USERDIR/.vim/dein/repos
 
@@ -10,3 +11,5 @@ chown $USERNAME:$USERNAME -R $USERDIR/.cache
 chmod 777 -R $USERDIR/.vim/dein
 chown $USERNAME:$USERNAME -R $USERDIR/.vim/dein
 
+cp /vagrant/.vagrant/machines/default/virtualbox/private_key $VAGRANTKEY
+chmod 600 $VAGRANTKEY
