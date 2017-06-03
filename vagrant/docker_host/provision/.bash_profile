9e9eaef53de3bc116897047619b028e31e113a74
tmn-o3 <tomorrow2009@live.jp>
2017-06-03 14:51:16 +0900

    fix vagrant up script

--- vagrant/docker_host/provision/.bash_profile
+++ vagrant/docker_host/provision/.bash_profile
@@ -3,4 +3,4 @@ if [ -f ~/.bashrc ]; then
     . ~/.bashrc
 fi
 
-export PATH=$PATH:$HOME/.local/bin:$HOME/bin:$HOME/app/neovim/bin
+export PATH=$PATH:$HOME/.local/bin:$HOME/bin:$HOME/app/neovim/bin:$HOME/app/phpctags/bin:$HOME/.config/composer/vendor/bin
--- vagrant/docker_host/provision/always.sh
+++ vagrant/docker_host/provision/always.sh
@@ -1,6 +1,7 @@
-cp /vagrant/provision/.bash_profile /home/vagrant/.bash_profile
-cp /vagrant/provision/.bashrc /home/vagrant/.bashrc
+cp -f /vagrant/provision/.bash_profile /home/vagrant/.bash_profile
+cp -f /vagrant/provision/.bashrc /home/vagrant/.bashrc
 if [ -x /usr/bin/Xvfb ] && [ -x /usr/bin/VBoxClient ] && [ ! -f /tmp/.X0-lock ]; then
     Xvfb -screen 0 320x240x8 > /dev/null 2>&1 &
+    sleep 0.5
     DISPLAY=:0 VBoxClient --clipboard
 fi
