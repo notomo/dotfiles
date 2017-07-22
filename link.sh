0ce2271a459166bd28033f8033721e76e2a3d632
tmn-o3 <tomorrow2009@live.jp>
2017-07-22 11:52:42 +0000

    add isort config

--- link.sh
+++ link.sh
@@ -6,45 +6,46 @@ then
 fi
 
 # vim
-ln -s ${install_root}/dotfiles/vim/rc/.vimrc ${install_root}/.vimrc
+ln -s "${install_root}"/dotfiles/vim/rc/.vimrc "${install_root}"/.vimrc
 # ln -s ${install_root}/dotfiles/vim/rc/.gvimrc ${install_root}/.gvimrc
 # ln -s ${install_root}/dotfiles/vim/rc/kaoriya/.vimrc_first.vim ${install_root}/.vimrc_first.vim
 # ln -s ${install_root}/dotfiles/vim/rc/kaoriya/.gvimrc_first.vim ${install_root}/.gvimrc_first.vim
 
-mkdir -p ${install_root}/.vim
-ln -s ${install_root}/dotfiles/vim/rc ${install_root}/.vim/rc
-ln -s ${install_root}/dotfiles/vim/autoload/ ${install_root}/.vim/autoload
-ln -s ${install_root}/dotfiles/vim/dict ${install_root}/.vim/dict
-ln -s ${install_root}/dotfiles/vim/snippets ${install_root}/.vim/snippets
-ln -s ${install_root}/dotfiles/vim/after ${install_root}/.vim/after
-ln -s ${install_root}/dotfiles/vim/ftplugin/ ${install_root}/.vim/ftplugin
-ln -s ${install_root}/dotfiles/vim/syntax/ ${install_root}/.vim/syntax
-ln -s ${install_root}/dotfiles/vim/indent/ ${install_root}/.vim/indent
-ln -s ${install_root}/dotfiles/vim/rplugin ${install_root}/.vim/rplugin
-ln -s ${install_root}/dotfiles/vim/plugin ${install_root}/.vim/plugin
+mkdir -p "${install_root}"/.vim
+ln -s "${install_root}"/dotfiles/vim/rc "${install_root}"/.vim/rc
+ln -s "${install_root}"/dotfiles/vim/autoload/ "${install_root}"/.vim/autoload
+ln -s "${install_root}"/dotfiles/vim/dict "${install_root}"/.vim/dict
+ln -s "${install_root}"/dotfiles/vim/snippets "${install_root}"/.vim/snippets
+ln -s "${install_root}"/dotfiles/vim/after "${install_root}"/.vim/after
+ln -s "${install_root}"/dotfiles/vim/ftplugin/ "${install_root}"/.vim/ftplugin
+ln -s "${install_root}"/dotfiles/vim/syntax/ "${install_root}"/.vim/syntax
+ln -s "${install_root}"/dotfiles/vim/indent/ "${install_root}"/.vim/indent
+ln -s "${install_root}"/dotfiles/vim/rplugin "${install_root}"/.vim/rplugin
+ln -s "${install_root}"/dotfiles/vim/plugin "${install_root}"/.vim/plugin
 
-mkdir -p ${install_root}/.vim/tmp/backup
-mkdir -p ${install_root}/.vim/tmp/undo
-mkdir -p ${install_root}/.vim/tmp/view
-mkdir -p ${install_root}/.vim/tmp/swap
-mkdir -p ${install_root}/.vim/reference
+mkdir -p "${install_root}"/.vim/tmp/backup
+mkdir -p "${install_root}"/.vim/tmp/undo
+mkdir -p "${install_root}"/.vim/tmp/view
+mkdir -p "${install_root}"/.vim/tmp/swap
+mkdir -p "${install_root}"/.vim/reference
 
 # neovim
-mkdir -p ${install_root}/.config
-ln -s ${install_root}/.vim ${install_root}/.config/nvim
-ln -s ${install_root}/dotfiles/vim/rc/init.vim ${install_root}/.config/nvim/init.vim
-# ln -s ${install_root}/dotfiles/vim/rc/ginit.vim ${install_root}/.config/nvim/ginit.vim
+mkdir -p "${install_root}"/.config
+ln -s "${install_root}"/.vim "${install_root}"/.config/nvim
+ln -s "${install_root}"/dotfiles/vim/rc/init.vim "${install_root}"/.config/nvim/init.vim
+# ln -s "${install_root}"/dotfiles/vim/rc/ginit.vim "${install_root}"/.config/nvim/ginit.vim
 
 # ideaVim
-# ln -s ${install_root}/dotfiles/idea/.ideavimrc ${install_root}/.ideavimrc
+# ln -s "${install_root}"/dotfiles/idea/.ideavimrc "${install_root}"/.ideavimrc
 
 # lint
-ln -s ${install_root}/dotfiles/lint/vim/.vintrc.yaml ${install_root}/.vintrc.yaml
-ln -s ${install_root}/dotfiles/lint/python/.flake8 ${install_root}/.flake8
+ln -s "${install_root}"/dotfiles/lint/vim/.vintrc.yaml "${install_root}"/.vintrc.yaml
+ln -s "${install_root}"/dotfiles/lint/python/.flake8 "${install_root}"/.flake8
+ln -s "${install_root}"/dotfiles/lint/python/.isort.cfg "${install_root}"/.isort.cfg
 
 # git
-ln -s ${install_root}/dotfiles/git/.gitignore_global ${install_root}/.gitignore_global
-cp ${install_root}/dotfiles/git/.gitconfig ${install_root}/.gitconfig
+ln -s "${install_root}"/dotfiles/git/.gitignore_global "${install_root}"/.gitignore_global
+cp "${install_root}"/dotfiles/git/.gitconfig "${install_root}"/.gitconfig
 
 if [ "$1" == "" ]
 then
