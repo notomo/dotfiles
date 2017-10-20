96cb129c1ad3e87302019f987f7de37bc67771f5
notomo <notomo.motono@gmail.com>
2017-10-20 20:08:06 +0900

    remove invalid commands

--- vagrant/docker_host/provision/always.sh
+++ vagrant/docker_host/provision/always.sh
@@ -1,4 +1,2 @@
 cp -f /vagrant/provision/.bash_profile ~/.bash_profile
 cp -f /vagrant/provision/.bashrc ~/.bashrc
-cp -f /vagrant/provision/lsyncd.conf /etc/lsyncd.conf
-cp -f /vagrant/provision/rsync_exclude.lst /etc/rsync_exclude.lst
