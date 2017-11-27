48f908cebf0b35cd41aafa67bbbdca693a6b0463
notomo <notomo.motono@gmail.com>
2017-11-27 21:15:31 +0900

    fix provision

--- vagrant/docker_host/provision/first_root.sh
+++ vagrant/docker_host/provision/first_root.sh
@@ -177,12 +177,12 @@ yum -y install xinetd
 
 mkdir -p "$BACKUP_FROM"
 mkdir -p "$BACKUP_TO"
-chown "$USERNAME:$USERNAME $BACKUP_FROM"
-chown "$USERNAME:$USERNAME $BACKUP_TO"
+chown $USERNAME:$USERNAME "$BACKUP_FROM"
+chown $USERNAME:$USERNAME "$BACKUP_TO"
 cp -f /vagrant/provision/lsyncd.conf /etc/lsyncd.conf
 sed -i 's#BACKUP_TO#'"$BACKUP_TO"'#g' /etc/lsyncd.conf
 cp -f /vagrant/provision/rsync_exclude.lst /etc/rsync_exclude.lst
-rsync -a --exclude-from=/vagrant/provision/rsync_exclude.lst "$BACKUP_FROM $USERDIR"
+rsync -a --exclude-from=/vagrant/provision/rsync_exclude.lst "$BACKUP_FROM" $USERDIR
 
 systemctl start xinetd
 systemctl enable xinetd
