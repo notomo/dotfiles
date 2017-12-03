6d3882e5059321b9f4f070918f619670ec3ed4e8
notomo <notomo.motono@gmail.com>
2017-12-04 08:28:22 +0900

    add tool

---
+++ tool/refresh_dotfiles_git.sh
@@ -0,0 +1,16 @@
+DOTFILES_DIR=~/dotfiles
+TMP_DIR=~/dotfiles_tmp
+pushd .
+rm -r $TMP_DIR
+mkdir -p $TMP_DIR
+cp -R $DOTFILES_DIR/* $TMP_DIR
+rm -r $DOTFILES_DIR
+git clone https://github.com/notomo/dotfiles.git $DOTFILES_DIR
+if [ -d $DOTFILES_DIR ]
+then
+    cp -R -u $TMP_DIR/* $DOTFILES_DIR/
+    rm -r $TMP_DIR
+    cd $DOTFILES_DIR
+    git reset --hard origin/master
+fi
+popd
