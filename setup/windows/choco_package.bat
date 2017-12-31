116cc087e0d0b25e407cc0b246a07b12a7e36c51
notomo <notomo.motono@gmail.com>
2018-01-01 08:51:21 +0900

    add windows nodejs provider support

--- setup/windows/choco_package.bat
+++ setup/windows/choco_package.bat
@@ -5,6 +5,8 @@ cd /d %~dp0
 C:\Python35\Scripts\pip3.exe install neovim >> choco_package.log 2>&1
 C:\Python27\Scripts\pip.exe install neovim >> choco_package.log 2>&1
 
+C:\PROGRA~1\nodejs\npm install -g neovim
+
 call addPath.wsf "C:\tools\msys64\usr\bin"
 
 echo "%PATH%" >> choco_package.log 2>&1
