24f9dfff9347585227c9cc6157a4a429b1260dc8
notomo <notomo.motono@gmail.com>
2018-04-15 09:18:03 +0900

    [add]mac setup sh(wip)

---
+++ setup/mac/setup_memo.sh
@@ -0,0 +1,14 @@
+
+# connect power supply
+xcode-select --install
+# click install
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# input password
+
+brew install ansible
+
+git clone https://github.com/notomo/dotfiles.git ~/dotfiles
+
+cd ~/dotfiles/ansible
+ansible-playbook playbooks/mac.yml
