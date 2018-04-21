c3f5548ab13f314f8f03d11696fb2ae78e713490
notomo <notomo.motono@gmail.com>
2018-04-22 00:13:40 +0900

    [fix]for vim8 settings

--- vim/rplugin/python3/denite/kind/curstr/altr/help.py
+++ vim/rplugin/python3/denite/kind/curstr/altr/help.py
@@ -1,28 +1,33 @@
 
-from curstr.action.group import Help
-from denite.kind.base import Kind as Base
+try:
+    from curstr.action.group import Help
+    from denite.kind.base import Kind as Base
 
+    class Kind(Base):
 
-class Kind(Base):
+        def __init__(self, vim):
+            super().__init__(vim)
 
-    def __init__(self, vim):
-        super().__init__(vim)
+            self.name = 'curstr/altr/help'
+            self.default_action = 'tabopen'
 
-        self.name = 'curstr/altr/help'
-        self.default_action = 'tabopen'
+        def action_open(self, context):
+            self._curstr(context, 'action_open')
 
-    def action_open(self, context):
-        self._curstr(context, 'action_open')
+        def action_tabopen(self, context):
+            self._curstr(context, 'action_tab_open')
 
-    def action_tabopen(self, context):
-        self._curstr(context, 'action_tab_open')
+        def action_vsplit(self, context):
+            self._curstr(context, 'action_vertical_open')
 
-    def action_vsplit(self, context):
-        self._curstr(context, 'action_vertical_open')
+        def _curstr(self, context, action_name):
+            targets = context['targets']
+            for target in targets:
+                word = target['word']
+                curstr_group = Help(self.vim, word)
+                getattr(curstr_group, action_name)()
+except ImportError:
+    from denite.kind.command import Kind as Command
 
-    def _curstr(self, context, action_name):
-        targets = context['targets']
-        for target in targets:
-            word = target['word']
-            curstr_group = Help(self.vim, word)
-            getattr(curstr_group, action_name)()
+    class Kind(Command):
+        pass
