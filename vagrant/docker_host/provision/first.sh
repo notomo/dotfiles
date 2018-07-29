7a04fa1a2c48f1528b35dc4bd86225e69dab3e72
notomo <notomo.motono@gmail.com>
2018-07-30 06:23:01 +0900

    [update]reduce provision

--- vagrant/docker_host/provision/first.sh
+++ vagrant/docker_host/provision/first.sh
@@ -31,7 +31,6 @@ rm php_manual_ja.tar.gz
 go get -u github.com/nsf/gocode
 go get -u golang.org/x/tools/cmd/goimports
 go get -u github.com/golang/lint/golint
-go get -u github.com/go-task/task/cmd/task
 go get -u golang.org/x/tools/cmd/gotype
 go get -u honnef.co/go/tools/cmd/gosimple
 go get -u honnef.co/go/tools/cmd/staticcheck
@@ -40,47 +39,9 @@ go get -u honnef.co/go/tools/cmd/unused
 go get -u github.com/mattn/mkup
 go get -u github.com/github/hub
 
-# ruby
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-sudo yum install -y gcc-6 bzip2 openssl-devel libyaml-devel libffi-devel readline-devel zlib-devel gdbm-devel ncurses-devel
-source ~/.bashrc
-rbenv install 2.4.2
-rbenv global 2.4.2
-gem install neovim
-gem install rubocop
-
 # neovim-remote
 pip3.5 install --user neovim-remote
 git config --global core.editor 'nvr --remote-wait-silent'
-
-# cd ~/app
-# wget -nv https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
-# unzip protoc-3.5.1-linux-x86_64.zip -d ~/app/protoc
-# cp ~/app/protoc/bin/protoc ~/.local/bin/protoc
-
-# groovy
-cd ~/app
-mkdir -p groovy
-cd groovy
-wget -nv --no-check-certificate https://dl.bintray.com/groovy/maven/groovy-binary-2.4.3.zip
-unzip groovy-binary-2.4.3.zip
-rm groovy-binary-2.4.3.zip
-ln -s groovy-2.4.3 ./latest
-
-# rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-rustup default nightly
-rustup component add rls-preview --toolchain nightly
-rustup component add rust-analysis --toolchain nightly
-rustup component add rust-src --toolchain nightly
-rustup run nightly cargo install rustfmt-nightly --force
-
-# dockerfile lint
-cd ~/app
-wget -nv --no-check-certificate https://github.com/hadolint/hadolint/releases/download/v1.2.5/hadolint-Linux-x86_64
-cp ./hadolint-Linux-x86_64 ~/.local/bin/hadolint
-chmod a+x ~/.local/bin/hadolint
 
 # nvim spell
 mkdir -p ~/.local/share/nvim/site/spell
@@ -91,15 +52,6 @@ wget http://ftp.vim.org/pub/vim/runtime/spell/en.utf-8.spl
 go get -d github.com/pocke/lemonade
 cd "$GOPATH"/src/github.com/pocke/lemonade/
 make install
-
-# haskell
-curl -sSL https://get.haskellstack.org/ | sh
-stack install hlint
-stack install hfmt
-cd ~/app
-git clone https://github.com/haskell/haskell-ide-engine
-cd haskell-ide-engine
-stack install
 
 # workspace
 mkdir -p ~/workspace
--- vagrant/docker_host/provision/first_root.sh
+++ vagrant/docker_host/provision/first_root.sh
@@ -1,7 +1,4 @@
 
-BACKUP_FROM=$1
-BACKUP_TO=$2
-
 USERNAME=vagrant
 USERDIR=/home/$USERNAME
 
@@ -18,7 +15,7 @@ yum -y install yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
 yum -y install docker-ce
-curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-"$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-"$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 usermod -aG docker $USERNAME
 systemctl status docker.service
@@ -37,7 +34,7 @@ yum -y install xorg-x11-server-Xvfb
 yum -y install wget
 yum -y install dh-autoreconf curl-devel expat-devel gettext-devel openssl-devel perl-devel zlib-devel
 yum -y install libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip
-GITVERSION=2.15.0
+GITVERSION=2.18.0
 wget -nv https://www.kernel.org/pub/software/scm/git/git-$GITVERSION.tar.gz
 tar -zxf git-$GITVERSION.tar.gz
 rm git-$GITVERSION.tar.gz
@@ -54,21 +51,6 @@ cd $NEOVIMDIR
 make CMAKE_BUILD_TYPE=Release
 make install
 
-# vim
-VIMDIR=$APPDIR/vim
-git clone https://github.com/neovim/neovim.git $VIMDIR
-cd $VIMDIR/src
-./configure \
---with-features=huge \
---enable-gui=gtk2 \
---enable-pythoninterp \
---with-python-config-dir=/usr/lib64/python2.7/config \
---enable-python3interp \
---with-python3-config-dir=/usr/lib64/python3.5/config-3.5m \
---enable-fail-if-missing
-make
-make install
-
 # ctags
 CTAGSDIR=$APPDIR/ctags
 git clone https://github.com/universal-ctags/ctags.git $CTAGSDIR
@@ -80,15 +62,11 @@ make install
 
 # pt
 cd $APPDIR
-wget -nv https://github.com/monochromegane/the_platinum_searcher/releases/download/v2.1.5/pt_linux_amd64.tar.gz
+wget -nv https://github.com/monochromegane/the_platinum_searcher/releases/download/v2.1.6/pt_linux_amd64.tar.gz
 tar zxf pt_linux_amd64.tar.gz
 mv pt_linux_amd64/pt /usr/local/bin/pt
 rm pt_linux_amd64.tar.gz
 rm -r pt_linux_amd64
-
-# ripgrep
-yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
-yum -y install ripgrep
 
 chown $USERNAME:$USERNAME -R $APPDIR
 
@@ -106,8 +84,6 @@ pip3.5 install mypy
 pip3.5 install autopep8
 pip3.5 install isort
 pip3.5 install yapf
-pip3.5 install lxml
-pip3.5 install python-language-server
 
 # python2
 yum -y install python-devel 
@@ -126,46 +102,14 @@ php composer-setup.php
 php -r "unlink('composer-setup.php');"
 mv composer.phar /usr/local/bin/composer
 
-# shell script
-yum install -y ShellCheck --enablerepo epel
-
 # golang
-GOVERSION=1.9.2
+GOVERSION=1.10.3
 wget -nv https://storage.googleapis.com/golang/go$GOVERSION.linux-amd64.tar.gz
 tar -C /usr/local -xzf go$GOVERSION.linux-amd64.tar.gz
 rm go$GOVERSION.linux-amd64.tar.gz
 
-# java
-wget -nv --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.rpm
-yum -y localinstall jdk-8u151-linux-x64.rpm
-rm jdk-8u151-linux-x64.rpm
-
-# plantuml
-wget -nv https://sourceforge.net/projects/plantuml/files/plantuml.jar
-
-# clang
-yum -y install clang lldb
-
-rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-yum-config-manager --add-repo http://download.mono-project.com/repo/centos/
-yum install -y mono-complete
-
-wget -nv --no-check-certificate https://nuget.org/nuget.exe
-mono nuget.exe update -self
-mv nuget.exe /usr/local/bin
-cp -f /vagrant/provision/nuget.sh /usr/local/bin/nuget
-cp -f /vagrant/provision/nugetinstall.sh /usr/local/bin/nugetinstall
-
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sh -c 'echo -e "[packages-microsoft-com-prod]\nname=packages-microsoft-com-prod \nbaseurl=https://packages.microsoft.com/yumrepos/microsoft-rhel7.3-prod\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/dotnetdev.repo'
-yum -y install libunwind libicu
-yum -y install dotnet-sdk-2.0.0
-
 # Vim script
 pip3.5 install vim-vint
-
-# sqlint
-gem install sqlint
 
 # javascript
 curl -sL https://rpm.nodesource.com/setup_9.x | bash -E -
@@ -174,17 +118,11 @@ npm install -g javascript-typescript-langserver
 
 npm i -g typescript
 npm i -g neovim
-npm i -g gulp
 npm i -g htmlhint
 npm i -g eslint
 npm i -g prettier
 npm i -g tslint
 npm i -g fixjson
-
-# lua
-yum install -y lua-devel
-yum install -y luarocks --enablerepo=epel
-luarocks install luacheck
 
 # text browser
 yum -y install lynx
@@ -192,24 +130,14 @@ yum -y install lynx
 # word dict
 yum -y install words
 
-# dotfiles for root
-git clone https://github.com/notomo/dotfiles.git ~/dotfiles
-sh ~/dotfiles/link.sh
-
 # sync
 sysctl fs.inotify.max_user_watches=32768
 echo fs.inotify.max_user_watches=32768 >> /etc/sysctl.conf
 yum -y install lsyncd --enablerepo=epel
 yum -y install xinetd
 
-mkdir -p "$BACKUP_FROM"
-mkdir -p "$BACKUP_TO"
-chown $USERNAME:$USERNAME "$BACKUP_FROM"
-chown $USERNAME:$USERNAME "$BACKUP_TO"
 cp -f /vagrant/provision/lsyncd.conf /etc/lsyncd.conf
-sed -i 's#BACKUP_TO#'"$BACKUP_TO"'#g' /etc/lsyncd.conf
 cp -f /vagrant/provision/rsync_exclude_list /etc/rsync_exclude_list
-rsync -a --exclude-from=/vagrant/provision/rsync_exclude_list "$BACKUP_FROM" $USERDIR
 
 systemctl start xinetd
 systemctl enable xinetd
@@ -223,22 +151,6 @@ chown $USERNAME:$USERNAME -R $USERDIR/.ssh/*
 chmod 0600 $USERDIR/.ssh/*
 chmod 0700 $USERDIR/.ssh
 
-# fish
-# yum install ncurses-devel
-# cd /etc/yum.repos.d/
-# wget -nv http://download.opensuse.org/repositories/shells:fish:release:2/CentOS_7/shells:fish:release:2.repo
-# yum -y install fish
-# cd $APPDIR
-
-# blender
-# wget -nv http://mirror.cs.umn.edu/blender.org/release/Blender2.78/blender-2.78c-linux-glibc219-x86_64.tar.bz2
-# tar xf blender-2.78c-linux-glibc219-x86_64.tar.bz2
-# rm blender-2.78c-linux-glibc219-x86_64.tar.bz2
-# mv blender-2.78c-linux-glibc219-x86_64 blender
-# yum -y install glib*
-# yum -y install libGLU
-# yum -y install libXi
-
 yum -y install tree
 
 yum -y install xdg-utils
@@ -248,12 +160,3 @@ then
     mv /usr/bin/xdg-open /usr/bin/xdg-open.tmp
 fi
 ln -s "$GOPATH"/bin/lemonade /usr/bin/xdg-open
-
-# tee /etc/yum.repos.d/highway.repo <<-EOF
-# [repos.highway]
-# name=highway
-# baseurl=http://tkengo.github.io/highway/fedora
-# enabled=0
-# gpgcheck=0
-# EOF
-# yum -y install highway --enablerepo="repos.highway"
