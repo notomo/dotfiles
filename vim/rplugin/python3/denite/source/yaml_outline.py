e580bb7938b701f00b88805d2fcb19d98f3c52fe
notomo <notomo.motono@gmail.com>
2019-07-03 21:44:48 +0900

    Fix yaml_outline source io error

--- vim/rplugin/python3/denite/source/yaml_outline.py
+++ vim/rplugin/python3/denite/source/yaml_outline.py
@@ -20,31 +20,31 @@ class Source(Base):
         with open(path, 'r') as f:
             loader = yaml.Loader(f)
 
-        candidates = []
+            candidates = []
 
-        def compose_node(parent, index):
-            line = loader.line
-            node = Composer.compose_node(loader, parent, index)
-            node.__line__ = line + 1
-            return node
+            def compose_node(parent, index):
+                line = loader.line
+                node = Composer.compose_node(loader, parent, index)
+                node.__line__ = line + 1
+                return node
 
-        loader.compose_node = compose_node
+            loader.compose_node = compose_node
 
-        def compose_mapping_node(anchor):
-            node = Composer.compose_mapping_node(loader, anchor)
-            for n in node.value:
-                candidates.append(create(n))
-            return node
+            def compose_mapping_node(anchor):
+                node = Composer.compose_mapping_node(loader, anchor)
+                for n in node.value:
+                    candidates.append(create(n))
+                return node
 
-        loader.compose_mapping_node = compose_mapping_node
+            loader.compose_mapping_node = compose_mapping_node
 
-        def create(node):
-            return {
-                'word': node[0].value,
-                'action__path': path,
-                'action__line': node[0].__line__,
-            }
+            def create(node):
+                return {
+                    'word': node[0].value,
+                    'action__path': path,
+                    'action__line': node[0].__line__,
+                }
 
-        loader.get_data()
+            loader.get_data()
 
-        return candidates
+            return candidates
