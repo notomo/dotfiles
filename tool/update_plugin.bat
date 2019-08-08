5cc35d0ba1d286cefa3418cf7e8ec4c2d4960cc8
notomo <notomo.motono@gmail.com>
2019-08-09 07:28:37 +0900

    Add update_plugin.bat

---
+++ tool/update_plugin.bat
@@ -0,0 +1,8 @@
+nvim ^
+    -c "runtime! plugin/rplugin.vim" ^
+    -u ~/dotfiles/vim/rc/minpac/minpac.vim ^
+    -i NONE ^
+    -c "call append(0, execute('call minpac#update()'))" ^
+    -c "%print" ^
+    -n --headless -c q!
+pause
