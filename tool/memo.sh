fa01caad23ee74aa5c8191efc07fabe52616975d
notomo <notomo.motono@gmail.com>
2019-10-31 09:46:11 +0900

    Add nvim ctags alias

--- tool/memo.sh
+++ tool/memo.sh
@@ -25,3 +25,4 @@ alias create_empty_commit='git commit --allow-empty -m'
 alias nvU="nvim -u ~/dotfiles/vim/local/test.vim --noplugin"
 alias vu="vim -u ~/dotfiles/vim/local/test.vim"
 alias rfl='sh ~/app/refresh_dotfiles_git.sh'
+alias nvimtags="ctags --languages=C,C++,Lua -R -I EXTERN -I INIT --exclude=.git src build/include build/src/nvim/auto .deps/build/src"
