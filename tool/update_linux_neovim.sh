4c4f22dbf3735fdac2aa188d640002e7ea5322d5
notomo <notomo.motono@gmail.com>
2019-11-22 07:12:30 +0900

    Fix update_linux_neovim.sh

--- tool/update_linux_neovim.sh
+++ tool/update_linux_neovim.sh
@@ -1,6 +1,10 @@
-nvim --version
-mv -f ~/.local/bin/nvim ~/.local/bin/nvim.tmp
-pushd ~/dotfiles/ansible
+#!/bin/bash
+
+$HOME/.local/bin/nvim --version
+mv -f $HOME/.local/bin/nvim $HOME/.local/bin/nvim.tmp
+mv -f $HOME/app/nvim $HOME/app/nvim.tmp
+mv -f $HOME/app/nvim.tar.gz $HOME/app/nvim.tar.gz.tmp
+pushd $HOME/dotfiles/ansible
 ANSIBLE_CONFIG=$(pwd) ansible-playbook playbooks/ubuntu.yml --tags neovim --ask-become-pass
-nvim --version
+$HOME/.local/bin/nvim --version
 popd
