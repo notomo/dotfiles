194fbc3509e381a533da8dcc41010efdf4a75d79
notomo <notomo.motono@gmail.com>
2020-08-16 17:37:07 +0900

    Add script for update windows neovim

---
+++ tool/update_windows_neovim.sh
@@ -0,0 +1,10 @@
+# on wsl
+set -exu
+
+host_home=`wslpath "$(wslvar USERPROFILE)"`
+
+cd ~/app
+rm nvim-win64.zip
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip
+unzip -o nvim-win64.zip
+cp -Rf Neovim ${host_home}/app/Neovim
