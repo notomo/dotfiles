711af043c50b86364dae77ca9baddc90af62f9a2
notomo <notomo.motono@gmail.com>
2020-11-09 21:58:56 +0900

    Adjust ansible

--- setup/mac/setup_memo.sh
+++ setup/mac/setup_memo.sh
@@ -1,5 +1,6 @@
 # connect power supply
 xcode-select --install
+# if error occurs, download Command Line Tools from https://developer.apple.com/download/more/?=command%20line%20tools
 # click install
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -10,4 +11,4 @@ brew install ansible
 git clone https://github.com/notomo/dotfiles.git ~/dotfiles
 
 cd ~/dotfiles/ansible
-ansible-playbook playbooks/mac.yml
+ansible-playbook playbooks/mac.yml --ask-become-pass
