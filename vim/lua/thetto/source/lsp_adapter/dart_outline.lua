fd9112e9ba21b5616052a72ef54db040d510ad4b
notomo <notomo.motono@gmail.com>
2021-02-23 21:28:26 +0900

    Fix dart outline

--- vim/lua/thetto/source/lsp_adapter/dart_outline.lua
+++ vim/lua/thetto/source/lsp_adapter/dart_outline.lua
@@ -15,17 +15,16 @@ function M.collect(self)
   return items
 end
 
-function M._to_items(self, item, parent_class, current_path)
-  local items = {}
-
+function M._to_items(self, item, parent, current_path)
   if item.kind == "NEW_INSTANCE" then
     return {}
   end
 
+  local items = {}
   local value = ""
   local desc = nil
   local pre_desc = ""
-  local class = nil
+  local next_parent = nil
   local element = item[self._element_key]
   if element then
     if element.kind == "FUNCTION" then
@@ -34,20 +33,40 @@ function M._to_items(self, item, parent_class, current_path)
       desc = ("%s%s%s"):format(pre_desc, value, element.parameters)
     elseif element.kind == "METHOD" then
       pre_desc = ("%s "):format(element.returnType)
-      value = ("%s.%s"):format(parent_class.name, element.name)
+      value = ("%s.%s"):format(parent.name, element.name)
+      desc = ("%s%s%s"):format(pre_desc, value, element.parameters)
+    elseif element.kind == "FUNCTION_TYPE_ALIAS" then
+      pre_desc = "typedef "
+      value = element.name
+      desc = ("%s%s = %s Function%s"):format(pre_desc, value, element.returnType, element.parameters)
+    elseif element.kind == "GETTER" then
+      pre_desc = ("%s get "):format(element.returnType)
+      value = ("%s.%s"):format(parent.name, element.name)
+      desc = ("%s%s"):format(pre_desc, value)
+    elseif element.kind == "SETTER" then
+      pre_desc = "set "
+      value = ("%s.%s"):format(parent.name, element.name)
       desc = ("%s%s%s"):format(pre_desc, value, element.parameters)
     elseif element.kind == "CONSTRUCTOR" then
       value = element.name
       desc = ("%s%s"):format(value, element.parameters)
     elseif element.kind == "FIELD" then
       pre_desc = ("%s "):format(element.returnType)
-      value = ("%s.%s"):format(parent_class.name, element.name)
+      value = ("%s.%s"):format(parent.name, element.name)
       desc = ("%s%s"):format(pre_desc, value)
+    elseif element.kind == "TOP_LEVEL_VARIABLE" then
+      value = element.name
+      desc = value
     elseif element.kind == "CLASS" then
       pre_desc = "class "
       value = element.name
       desc = ("%s%s"):format(pre_desc, value)
-      class = element
+      next_parent = element
+    elseif element.kind == "EXTENSION" then
+      pre_desc = "extension "
+      value = element.name
+      desc = ("%s%s"):format(pre_desc, value)
+      next_parent = element
     elseif element.kind == "CONSTRUCTOR_INVOCATION" then
       return {}
     end
@@ -68,7 +87,7 @@ function M._to_items(self, item, parent_class, current_path)
   })
 
   for _, v in ipairs(item.children or {}) do
-    vim.list_extend(items, M._to_items(self, v, class, current_path))
+    vim.list_extend(items, M._to_items(self, v, next_parent, current_path))
   end
 
   return items
