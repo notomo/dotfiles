e6745e6613ee709b9fc735c0fe5b831d6003902c
notomo <notomo.motono@gmail.com>
2021-03-31 20:49:26 +0900

    Update neovim_dev task

--- tool/update_linux_neovim.sh
+++ tool/update_linux_neovim.sh
@@ -5,6 +5,6 @@ mv -f $HOME/.local/bin/nvim $HOME/.local/bin/nvim.tmp
 mv -f $HOME/app/nvim $HOME/app/nvim.tmp
 mv -f $HOME/app/nvim.tar.gz $HOME/app/nvim.tar.gz.tmp
 pushd $HOME/dotfiles/ansible
-ANSIBLE_CONFIG=$(pwd) ansible-playbook playbooks/ubuntu.yml --tags neovim --ask-become-pass
+ANSIBLE_CONFIG=$(pwd) ansible-playbook playbooks/ubuntu.yml --tags neovim
 $HOME/.local/bin/nvim --version
 popd
