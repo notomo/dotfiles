bc6c9963ab48032d7e746833dcd60b35d06c4191
notomo <notomo.motono@gmail.com>
2021-10-15 22:37:27 +0900

    Add optpack source

---
+++ vim/lua/thetto/handler/source/optpack.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+function M.collect()
+  local items = {}
+  local plugins = require("optpack").list()
+  for _, plugin in ipairs(plugins) do
+    table.insert(items, {value = plugin.full_name, path = plugin.directory})
+  end
+  return items
+end
+
+M.kind_name = "file/directory"
+
+return M
