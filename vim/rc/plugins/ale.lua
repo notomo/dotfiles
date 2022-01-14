32215583e80fa67bf6ce44b0ce2947915ac9e7ee
notomo <notomo.motono@gmail.com>
2022-01-14 09:29:44 +0900

    Fix mapping

--- vim/rc/plugins/ale.lua
+++ vim/rc/plugins/ale.lua
@@ -10,9 +10,9 @@ vim.g.ale_lint_on_insert_leave = 0
 vim.g.ale_hover_cursor = 0
 vim.g.ale_disable_lsp = 1
 
-vim.g.ale_linters = {}
+vim.g.ale_linters = vim.empty_dict()
 
-local fixers = {}
+local fixers = vim.empty_dict()
 fixers["sh"] = { "shfmt" }
 fixers["python"] = { "black", "isort" }
 fixers["go"] = { "goimports" }
@@ -31,6 +31,6 @@ fixers["dart"] = { "dart-format" }
 fixers["c"] = { "uncrustify" }
 vim.g.ale_fixers = fixers
 
-vim.g.ale_c_uncrustify_options = "-l c -c " .. vim.fn.expand("~/workspace/neovim/src/uncrustify.cfg")
+vim.g.ale_c_uncrustify_options = "-c " .. vim.fn.expand("~/workspace/neovim/src/uncrustify.cfg")
 vim.g.ale_lua_stylua_options = "--config-path " .. vim.fn.expand("~/dotfiles/tool/stylua.toml")
 vim.g.ale_sql_sqlfmt_options = "-u"
