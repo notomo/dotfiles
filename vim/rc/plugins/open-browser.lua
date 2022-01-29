8bca2f64b57a3a9b83b65662046640e95814bbca
notomo <notomo.motono@gmail.com>
2022-01-29 21:40:13 +0900

    Add browser open mapping

--- vim/rc/plugins/open-browser.lua
+++ vim/rc/plugins/open-browser.lua
@@ -3,6 +3,7 @@ require("notomo.mapping").set_prefix({ "n", "x" }, "browser", "[exec]b")
 vim.keymap.set("n", "[browser]s", [[<Cmd>execute 'OpenBrowserSearch' expand('<cword>')<CR>]])
 vim.keymap.set("n", "[browser]o", [[<Cmd>execute 'OpenBrowser' expand('<cWORD>')<CR>]])
 vim.keymap.set("n", "[browser]i", [[:<C-u>OpenBrowserSearch<Space>]])
+vim.keymap.set("n", "[browser]y", [[<Cmd>execute 'OpenBrowser' getreg('+')<CR>]])
 
 if not vim.fn.empty(vim.env.SSH_CLIENT) and vim.fn.has("mac") == 1 and vim.fn.executable("lemonade") then
   vim.g.openbrowser_browser_commands = { { name = "lemonade", args = "lemonade open {uri}" } }
