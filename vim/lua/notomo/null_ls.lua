7822c36b7f3d699bb97e09cdd73d36eb2607fa26
notomo <notomo.motono@gmail.com>
2022-02-09 22:04:05 +0900

    Use null-ls builtin actionlint

--- vim/lua/notomo/null_ls.lua
+++ vim/lua/notomo/null_ls.lua
@@ -1,23 +1,6 @@
 local null_ls = require("null-ls")
-local helpers = require("null-ls.helpers")
-
-local actionlint = {
-  method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-  filetypes = { "yaml" },
-  generator = null_ls.generator({
-    command = "actionlint",
-    args = { "-oneline" },
-    to_stdin = false,
-    format = "line",
-    check_exit_code = function(code)
-      return code == 0 or code == 1
-    end,
-    on_output = helpers.diagnostics.from_pattern([=[^[^:]+:(%d+):(%d+): (.*)]=], {
-      "row",
-      "col",
-      "message",
-    }),
-  }),
-}
-null_ls.register(actionlint)
-null_ls.setup({})
+null_ls.setup({
+  sources = {
+    null_ls.builtins.diagnostics.actionlint,
+  },
+})
