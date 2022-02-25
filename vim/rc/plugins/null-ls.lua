e7dafe3975c011f9ee004ed0de5f323e1ba44e03
notomo <notomo.motono@gmail.com>
2022-02-25 20:57:56 +0900

    Fix null-ls should_attach

--- vim/rc/plugins/null-ls.lua
+++ vim/rc/plugins/null-ls.lua
@@ -3,4 +3,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.actionlint,
   },
+  should_attach = function(bufnr)
+    return vim.api.nvim_buf_get_name(bufnr):match("/%.github/")
+  end,
 })
