f5408634af084299fd1e13a30db214b5b1db67b2
notomo <notomo.motono@gmail.com>
2022-04-05 09:00:00 +0900

    Add zx mapping

--- vim/after/ftplugin/javascript.lua
+++ vim/after/ftplugin/javascript.lua
@@ -3,3 +3,9 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.expandtab = true
 require("notomo.mapping").npm()
+
+if vim.endswith(vim.fn.bufname("%"), ".mjs") then
+  vim.keymap.set("n", "<Leader>Q", function()
+    require("cmdhndlr").run({ name = "javascript/zx" })
+  end, { buffer = true })
+end
