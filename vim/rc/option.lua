a0a0f1919bcc7406eadf595ae38390dc7a51fa68
notomo <notomo.motono@gmail.com>
2022-05-02 21:45:06 +0900

    Remove unneeded option setting

--- vim/rc/option.lua
+++ vim/rc/option.lua
@@ -2,7 +2,6 @@ local vim = vim
 local opt = vim.opt
 local g = vim.g
 
-opt.encoding = "utf-8"
 vim.env.LANG = "en_US.UTF-8"
 
 g.no_plugin_maps = 1
@@ -50,61 +49,42 @@ opt.showtabline = 2
 opt.signcolumn = "yes"
 opt.ignorecase = true
 opt.smartcase = true
-opt.hlsearch = true
-opt.spell = false
 opt.startofline = true
 opt.lazyredraw = true
-opt.autoindent = true
 opt.ruler = false
 opt.number = true
-opt.backspace = "indent,eol,start"
 opt.whichwrap = "b,s,h,l,<,>,[,]"
 opt.cursorline = true
 opt.listchars = [[tab:»-,trail:-,eol:↲,extends:»,precedes:«,nbsp:%]]
 opt.list = true
-opt.laststatus = 2
 opt.cmdheight = 2
 opt.scrolloff = 8
 opt.sidescrolloff = 24
 opt.showmatch = true
-opt.wildmenu = true
 opt.wildmode = "longest:full"
-opt.showcmd = true
-opt.autoread = true
-opt.hidden = true
 opt.switchbuf = "useopen"
-opt.textwidth = 0
 opt.matchpairs:append("<:>")
 opt.shiftwidth = 0
 opt.softtabstop = 4
 opt.tabstop = 4
-opt.smarttab = true
 opt.clipboard = "unnamed,unnamedplus"
-opt.foldmethod = "manual"
 opt.showmode = false
-opt.conceallevel = 0
 opt.shortmess:append("I")
 opt.shortmess:remove("S")
-opt.visualbell = false
 opt.diffopt:append("vertical")
 opt.diffopt:append("iwhite")
 opt.diffopt:append("hiddenoff")
 opt.diffopt:append("algorithm:histogram")
 opt.mouse = "a"
 opt.grepprg = [[git\ grep\ -n\ $*]]
-opt.wrapscan = true
 opt.backupdir = vim.fn.expand("~/.vim/tmp/backup/")
-opt.undofile = false
 opt.swapfile = false
-opt.title = false
 opt.expandtab = true
-opt.foldenable = true
 opt.foldlevel = 3
 vim.cmd([[set wildcharm=<C-z>]])
 opt.undoreload = 0
 opt.updatecount = 0
 opt.keywordprg = ":help"
-opt.helplang = "ja"
 opt.sessionoptions:remove("blank")
 opt.sessionoptions:remove("buffers")
 opt.spelllang = "en,cjk"
@@ -119,10 +99,8 @@ opt.termguicolors = true
 opt.spelloptions = "camel"
 opt.cedit = [[<C-q>]]
 opt.cmdwinheight = 12
-opt.inccommand = "nosplit"
 opt.guicursor =
   [[n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175]]
-opt.wildoptions:append("pum")
 opt.pumblend = 15
 opt.shada = [['10,!,<50,/0,s10,h,@10,:100]]
 if vim.fn.has("win32") ~= 1 then
