fb70d89343c6c870298a8df8610d384b612250d9
notomo <notomo.motono@gmail.com>
2022-05-07 23:04:59 +0900

    Move rc

--- vim/lua/notomo/init.lua
+++ vim/lua/notomo/init.lua
@@ -5,9 +5,9 @@ local safe_cmd = function(cmd)
   end
 end
 
-safe_cmd([[runtime! rc/local/*.lua]])
+safe_cmd([[runtime! lua/notomo/local/*.lua]])
 safe_cmd([[luafile ~/dotfiles/vim/lua/notomo/option.lua]])
 safe_cmd([[luafile ~/dotfiles/vim/lua/notomo/autocmd.lua]])
 safe_cmd([[luafile ~/dotfiles/vim/lua/notomo/mapping/init.lua]])
 safe_cmd([[luafile ~/dotfiles/vim/lua/notomo/plugin/_manager.lua]])
-safe_cmd([[runtime! rc/local/after/*.lua]])
+safe_cmd([[runtime! lua/notomo/local/after/*.lua]])
