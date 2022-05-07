16f92825bc377ec59a70a81b9165c223fcdb1ed1
notomo <notomo.motono@gmail.com>
2022-05-07 23:29:08 +0900

    Use set_hl

--- vim/syntax/mytodo.lua
+++ vim/syntax/mytodo.lua
@@ -2,9 +2,7 @@ if vim.b.current_syntax then
   return
 end
 
-vim.cmd([[
-syntax match mytodoDone "^\s*#.*"
-highlight default link mytodoDone Comment
-]])
+vim.cmd([[syntax match mytodoDone "^\s*#.*"]])
+vim.api.nvim_set_hl(0, "mytodoDone", { default = true, link = "Comment" })
 
 vim.b.current_syntax = "mytodo"
