9bf4990dd0e8ae55ec08d051922211f759405209
notomo <notomo.motono@gmail.com>
2022-05-19 23:18:13 +0900

    Improve format

--- vim/lua/notomo/lsp/autocmd.lua
+++ vim/lua/notomo/lsp/autocmd.lua
@@ -1,21 +1,32 @@
 local M = {}
 
+local format = function(bufnr)
+  local client = vim.lsp.get_active_clients({ name = "null-ls" })[1]
+  if not client then
+    return require("misclib.message").warn("no null-ls client")
+  end
+
+  local params = vim.lsp.util.make_formatting_params()
+  local method = "textDocument/formatting"
+  client.request(method, params, function(...)
+    local handler = client.handlers[method] or vim.lsp.handlers[method]
+    handler(...)
+    vim.api.nvim_buf_call(bufnr, function()
+      return vim.cmd([[silent noautocmd update]])
+    end)
+  end, bufnr)
+end
+
 function M.setup()
   require("null-ls") -- to lazy load
   local group = vim.api.nvim_create_augroup("notomo_lsp_format", { clear = false })
-  local events = { "BufWritePre" }
-  vim.api.nvim_clear_autocmds({ event = events, buffer = 0, group = group })
+  local events = { "BufWritePost" }
+  vim.api.nvim_clear_autocmds({ buffer = 0, group = group })
   vim.api.nvim_create_autocmd(events, {
     buffer = 0,
     group = group,
-    callback = function()
-      vim.lsp.buf.format({
-        filter = function(clients)
-          return vim.tbl_filter(function(client)
-            return client.name == "null-ls"
-          end, clients)
-        end,
-      })
+    callback = function(args)
+      format(args.buf)
     end,
   })
 end
