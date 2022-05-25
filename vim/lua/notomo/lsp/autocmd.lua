e519e874c66840de8909df35aaddb01a3955bec5
notomo <notomo.motono@gmail.com>
2022-05-25 22:00:54 +0900

    Async format with reloading

--- vim/lua/notomo/lsp/autocmd.lua
+++ vim/lua/notomo/lsp/autocmd.lua
@@ -1,11 +1,23 @@
 local M = {}
 
-local format = function(bufnr)
-  local client = vim.lsp.get_active_clients({ name = "null-ls" })[1]
-  if not client then
-    return require("misclib.message").warn("no null-ls client")
+local enable = function(client, bufnr)
+  local path = vim.api.nvim_buf_get_name(bufnr)
+
+  local root_dir = client.config.root_dir
+  if not vim.endswith(root_dir, "/") then
+    root_dir = root_dir .. "/"
   end
 
+  if vim.startswith(path, root_dir) then
+    return false
+  end
+
+  vim.lsp.stop_client(client.id)
+  require("null-ls").enable({ method = require("null-ls").methods.FORMATTING })
+  return true
+end
+
+local _format = function(client, bufnr)
   local params = vim.lsp.util.make_formatting_params()
   local method = "textDocument/formatting"
   client.request(method, params, function(...)
@@ -15,6 +27,33 @@ local format = function(bufnr)
       return vim.cmd([[silent noautocmd update]])
     end)
   end, bufnr)
+end
+
+local format = function(bufnr)
+  local null_ls_client = vim.lsp.get_active_clients({ name = "null-ls" })[1]
+  if not null_ls_client then
+    return require("misclib.message").warn("no null-ls client")
+  end
+
+  local reloading = enable(null_ls_client, bufnr)
+  if not reloading then
+    return _format(null_ls_client, bufnr)
+  end
+
+  local group = vim.api.nvim_create_augroup("notomo_lsp_format_async", { clear = false })
+  vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+  vim.api.nvim_create_autocmd({ "LspAttach" }, {
+    group = group,
+    buffer = bufnr,
+    callback = function()
+      local client = vim.lsp.get_active_clients({ name = "null-ls" })[1]
+      if not client then
+        return
+      end
+      _format(client, bufnr)
+      return true
+    end,
+  })
 end
 
 function M.setup()
