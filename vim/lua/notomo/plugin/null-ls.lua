665e76277828a908f6d5243fc260c3e5f6417d7e
notomo <notomo.motono@gmail.com>
2022-06-14 21:43:25 +0900

    Adjust stylua for neovim dev

--- vim/lua/notomo/plugin/null-ls.lua
+++ vim/lua/notomo/plugin/null-ls.lua
@@ -22,7 +22,13 @@ null_ls.setup({
     null_ls.builtins.formatting.terraform_fmt,
     null_ls.builtins.formatting.dart_format,
     null_ls.builtins.formatting.stylua.with({
-      extra_args = { "--config-path", vim.fn.expand("~/dotfiles/tool/stylua.toml") },
+      extra_args = function(params)
+        local config = vim.fn.expand("~/dotfiles/tool/stylua.toml")
+        if vim.startswith(params.bufname, vim.fn.expand("~/workspace/neovim/")) then
+          config = vim.fn.expand("~/workspace/neovim/.stylua.toml")
+        end
+        return { "--config-path", config }
+      end,
     }),
     null_ls.builtins.formatting.uncrustify.with({
       extra_args = { "-c", vim.fn.expand("~/workspace/neovim/src/uncrustify.cfg") },
