911f503080080a050955bfedf5e4c690a2814aaf
notomo <notomo.motono@gmail.com>
2022-08-15 08:31:50 +0900

    Remove unused vim/indent

--- vim/lua/notomo/vim.lua
+++ vim/lua/notomo/vim.lua
@@ -18,36 +18,4 @@ function M._move(to_next)
   vim.fn.search("\\v^\\s*\\zsfu(nction)?(!)?", flags)
 end
 
-function M.indent()
-  local current_line = vim.fn.getline(".")
-  if (vim.fn.col(".") - 1) ~= vim.fn.matchend(current_line, "^\\s*") then
-    return -1
-  end
-
-  local row = vim.fn.prevnonblank(vim.v.lnum - 1)
-  local plus_one = vim.fn.indent(row) + vim.fn.shiftwidth()
-  local line = vim.fn.getline(row)
-  if M._match(line, "\\v.*\\(\\s*$") then
-    -- e.g. func(
-    return plus_one
-  elseif M._match(line, "\\v^\\s*(fu|if|else|for|while)") then
-    -- e.g. function!
-    return plus_one
-  elseif M._match(line, "\\v.*:$") then
-    -- e.g. def func():
-    return plus_one
-  elseif M._match(line, "\\v.*\\[\\s*$") then
-    -- e.g. array = [
-    return plus_one
-  elseif M._match(line, "\\v.*\\{\\s*$") then
-    -- e.g. dict = {
-    return plus_one
-  end
-  return -1
-end
-
-function M._match(str, pattern)
-  return vim.fn.match(str, pattern) ~= -1
-end
-
 return M
