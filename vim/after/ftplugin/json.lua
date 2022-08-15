9e49de526a2fb775b2aad309c124cdcc8f3042e7
notomo <notomo.motono@gmail.com>
2022-08-16 08:02:58 +0900

    Improve json mapping

--- vim/after/ftplugin/json.lua
+++ vim/after/ftplugin/json.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("n", "J", [[<Cmd>lua require("notomo.json").next()<CR>]], { buffer = true })
-vim.keymap.set("n", "K", [[<Cmd>lua require("notomo.json").prev()<CR>]], { buffer = true })
+vim.keymap.set({ "n", "x" }, "J", [[<Cmd>lua require("notomo.json").next()<CR>]], { buffer = true })
+vim.keymap.set({ "n", "x" }, "K", [[<Cmd>lua require("notomo.json").prev()<CR>]], { buffer = true })
 if vim.fn.bufname("%") == "package.json" then
   require("notomo.npm").mapping()
 end
