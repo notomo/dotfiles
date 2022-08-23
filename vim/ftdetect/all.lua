28b59f62acab5e11c54a33718a582cf3d59779b2
notomo <notomo.motono@gmail.com>
2022-08-24 07:20:02 +0900

    Adjust ftdetect

--- vim/ftdetect/all.lua
+++ vim/ftdetect/all.lua
@@ -1,11 +1,5 @@
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = { "go.mod" },
-  callback = function()
-    vim.bo.filetype = "gomod"
-  end,
-})
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = { "*.mk.*", "*.mk" },
+  pattern = { "*.mk.*" },
   callback = function()
     vim.bo.filetype = "make"
   end,
