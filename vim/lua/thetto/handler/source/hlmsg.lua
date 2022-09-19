60224e29161a3870ad19df703647c9638b575816
notomo <notomo.motono@gmail.com>
2022-09-19 12:08:49 +0000

    Fix thetto hlmsg source

--- vim/lua/thetto/handler/source/hlmsg.lua
+++ vim/lua/thetto/handler/source/hlmsg.lua
@@ -8,7 +8,7 @@ function M.collect()
     local start_col = 0
     for _, pair in ipairs(chunk) do
       local text, hl_group = unpack(pair)
-      local end_col = start_col + vim.fn.strdisplaywidth(text)
+      local end_col = start_col + #text
       table.insert(texts, text)
       table.insert(highlights, {
         group = hl_group or "Normal",
@@ -30,7 +30,7 @@ M.kind_name = "word"
 function M.highlight(highlighter, items, first_line)
   for i, item in ipairs(items) do
     for _, highlight in ipairs(item.highlights) do
-      highlighter:add(highlight.group, first_line + i - 1, highlight.start_col, highlight.end_col)
+      highlighter:highlight(highlight.group, first_line + i - 1, highlight.start_col, highlight.end_col)
     end
   end
 end
