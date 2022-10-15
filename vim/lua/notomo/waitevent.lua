12dcee55dceeabe8fc965388ae3cac07795a6a07
notomo <notomo.motono@gmail.com>
2022-10-15 02:51:13 +0000

    Support waitevent with wq

--- vim/lua/notomo/waitevent.lua
+++ vim/lua/notomo/waitevent.lua
@@ -20,25 +20,33 @@ function M.setup(addr, path)
   end
 
   local group = vim.api.nvim_create_augroup("waitevent_" .. vim.fn.bufnr(), {})
+  local done = false
+
   vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     group = group,
     buffer = 0,
     callback = function()
       vim.fn.chansend(ch, "done")
+      done = true
       vim.api.nvim_clear_autocmds({ group = group })
-      if vim.api.nvim_win_is_valid(window_id) then
-        vim.api.nvim_win_close(window_id, true)
-      end
-      back()
+      vim.schedule(function()
+        if vim.api.nvim_win_is_valid(window_id) then
+          vim.api.nvim_win_close(window_id, true)
+        end
+        back()
+      end)
     end,
   })
+
   vim.api.nvim_create_autocmd({ "BufDelete", "BufWipeout" }, {
     group = group,
     buffer = 0,
     callback = function()
-      vim.fn.chansend(ch, "cancel")
-      vim.api.nvim_clear_autocmds({ group = group })
-      back()
+      if not done then
+        vim.fn.chansend(ch, "cancel")
+        vim.api.nvim_clear_autocmds({ group = group })
+        back()
+      end
     end,
   })
 
