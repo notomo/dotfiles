693874e88c8d89631cb733aa9679873dd1a6e2ff
notomo <notomo.motono@gmail.com>
2022-11-12 17:40:35 +0900

    Add adjust setup script

---
+++ setup/ubuntu/adjust.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gh auth login --git-protocol=ssh --hostname=GitHub.com
+[ ! -d ~/workspace/todo ] && gh repo clone notomo/todo ~/workspace/todo
+
+[ ! -f ~/dotfiles/vim/lua/notomo/local/local.lua ] && cp ~/dotfiles/vim/lua/notomo/local/local.lua.sample ~/dotfiles/vim/lua/notomo/local/local.lua
+
+ANSIBLE_CONFIG=~/dotfiles/ansible ansible-playbook ~/dotfiles/ansible/playbooks/ubuntu.yml --ask-become-pass --tags gitconfig --tags default_shell
