b4fc72b049e1a5b32f154efccb5ef78c8a074f4f
notomo <notomo.motono@gmail.com>
2022-11-26 11:42:52 +0900

    Remove unused gina settings

--- vim/lua/notomo/plugin/gina/init.lua
+++ vim/lua/notomo/plugin/gina/init.lua
@@ -1,17 +1,10 @@
-vim.g["gina#command#blame#use_default_aliases"] = 0
-vim.g["gina#command#changes#use_default_aliases"] = 0
-
 vim.fn["gina#custom#command#option"]("show", "--group", "show")
-vim.fn["gina#custom#command#option"]("changes", "--group", "changes")
-vim.fn["gina#custom#command#option"]("commit", "--group", "short")
-
-vim.fn["gina#custom#command#option"]([[/\%(changes\|ls\|commit\)]], "--opener", "botright split")
-vim.fn["gina#custom#command#option"]([[/\%(diff\|blame\|compare\|patch\)]], "--opener", "tabedit")
-
 vim.g["gina#core#console#enable_message_history"] = 1
 
 vim.api.nvim_create_augroup("gina_setting", {})
 
+vim.g["gina#command#blame#use_default_aliases"] = 0
+vim.fn["gina#custom#command#option"]([[/\%(blame\)]], "--opener", "tabedit")
 vim.g["gina#command#blame#formatter#format"] = "%su%=on %ti by %au %ma%in"
 vim.g["gina#command#blame#formatter#timestamp_format1"] = "%Y-%m-%d"
 vim.g["gina#command#blame#formatter#timestamp_format2"] = "%Y-%m-%d"
@@ -24,73 +17,11 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.keymap.set({ "n" }, "j", "j<Plug>(gina-blame-echo)", { buffer = true })
     vim.keymap.set({ "n" }, "k", "k<Plug>(gina-blame-echo)", { buffer = true })
     vim.keymap.set({ "n" }, "<CR>", [[:call gina#action#call('show:commit:tab')<CR>]], { buffer = true, silent = true })
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = "gina_setting",
-  pattern = { "diff" },
-  callback = function()
-    vim.keymap.set("n", "sgj", function()
-      require("notomo.edit").to_prev_syntax("diffLine", 1, 1)
-    end, { buffer = true, silent = true })
-    vim.keymap.set("n", "sgk", function()
-      require("notomo.edit").to_prev_syntax("diffLine", 1, -1)
-    end, { buffer = true, silent = true })
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = "gina_setting",
-  pattern = { "gina-commit" },
-  callback = function()
-    vim.keymap.set({ "n" }, "[file]w", ":wq<CR>", { buffer = true })
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = "gina_setting",
-  pattern = { "gina-blame" },
-  callback = function()
-    vim.keymap.set(
-      { "n" },
-      "cc",
-      [[:call gina#action#call("changes:of:rightest")<CR>]],
-      { buffer = true, silent = true }
-    )
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = "gina_setting",
-  pattern = { "gina-blame" },
-  callback = function()
     vim.keymap.set({ "n" }, "dd", [[:call gina#action#call("compare")<CR>]], { buffer = true, silent = true })
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = "gina_setting",
-  pattern = { "gina-blame" },
-  callback = function()
     vim.keymap.set({ "n" }, "D", [[:call gina#action#call("diff")<CR>]], { buffer = true, silent = true })
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = "gina_setting",
-  pattern = { "gina-ls", "gina-blame", "gina-changes" },
-  callback = function()
     vim.keymap.set({ "n" }, "o", [[:call gina#action#call("show")<CR>]], { buffer = true, silent = true })
     vim.keymap.set({ "n" }, "t<Space>", [[:call gina#action#call("show:tab")<CR>]], { buffer = true, silent = true })
     vim.keymap.set({ "n" }, "sv", [[:call gina#action#call("show:rightest")<CR>]], { buffer = true, silent = true })
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = "gina_setting",
-  pattern = { "gina-ls", "gina-blame", "gina-changes" },
-  callback = function()
     vim.keymap.set({ "n" }, "<CR>", [[:call gina#action#call("show:tab")<CR>]], { buffer = true, silent = true })
   end,
 })
--- vim/lua/notomo/plugin/gina/mapping.lua
+++ vim/lua/notomo/plugin/gina/mapping.lua
@@ -8,14 +8,20 @@ function M.setup()
   vim.keymap.set("n", "[yank]U", [[<Cmd>Gina browse : --yank<CR>:echomsg 'yank ' . @+<CR>]])
   vim.keymap.set("x", "[yank]U", [[:Gina browse : --yank --exact<CR>:echomsg 'yank ' . @+<CR>]])
 
+  local relpath = function()
+    local git = vim.fn["gina#core#get_or_fail"]()
+    local abspath = vim.fn["gina#core#repo#abspath"](git, "")
+    local curpath = vim.fn.substitute(vim.fn.expand("%:p"), "\\", "/", "g")
+    return vim.fn.substitute(curpath, abspath, "", "")
+  end
   vim.keymap.set("n", "[git]B", function()
-    return ":<C-u>Gina blame :" .. require("notomo.plugin.gina.util").relpath() .. "<CR>"
+    return ":<C-u>Gina blame :" .. relpath() .. "<CR>"
   end, { expr = true })
   vim.keymap.set("n", "[git]dd", function()
-    return ":<C-u>Gina compare :" .. require("notomo.plugin.gina.util").relpath() .. "<CR>"
+    return ":<C-u>Gina compare :" .. relpath() .. "<CR>"
   end, { expr = true })
   vim.keymap.set("n", "[git]df", function()
-    return ":<C-u>Gina diff :" .. require("notomo.plugin.gina.util").relpath() .. "<CR>"
+    return ":<C-u>Gina diff :" .. relpath() .. "<CR>"
   end, { expr = true })
 end
 
