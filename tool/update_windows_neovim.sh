c69bb8ef9c84ea8b70115e0323353f3cd5da89f6
notomo <notomo.motono@gmail.com>
2022-12-02 10:17:13 +0900

    Fix windows nvim install

--- tool/update_windows_neovim.sh
+++ tool/update_windows_neovim.sh
@@ -1,10 +1,11 @@
 # on wsl
 set -exu
 
-host_home=`wslpath "$(wslvar USERPROFILE)"`
+host_home=`wslpath "$(wslvar -s USERPROFILE)"`
 
 cd ~/app
 rm -f nvim-win64.zip
 wget https://github.com/neovim/neovim/releases/download/nightly/nvim-win64.zip
 unzip -o nvim-win64.zip
+rm -rf ${host_home}/app/nvim-win64
 cp -Rf nvim-win64 ${host_home}/app/
