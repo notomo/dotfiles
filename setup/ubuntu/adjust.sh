3500e60d9d8a523278e4c0574c108d2df5bfdaa3
notomo <notomo.motono@gmail.com>
2022-12-10 19:41:30 +0900

    Adjust gh init

--- setup/ubuntu/adjust.sh
+++ setup/ubuntu/adjust.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gh auth login --git-protocol=ssh --hostname=GitHub.com
+gh auth login --git-protocol=ssh --hostname=gitHub.com
 [ ! -d ~/workspace/todo ] && gh repo clone notomo/todo ~/workspace/todo
 
 [ ! -f ~/dotfiles/vim/lua/notomo/local/local.lua ] && cp ~/dotfiles/vim/lua/notomo/local/local.lua.sample ~/dotfiles/vim/lua/notomo/local/local.lua
