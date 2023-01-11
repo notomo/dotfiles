28f2f8b8eeb84e1ac25151748a097eaeb68b60f2
notomo <notomo.motono@gmail.com>
2023-01-11 22:51:14 +0900

    Adjust ocaml runner option

--- vim/after/ftplugin/ocaml.lua
+++ vim/after/ftplugin/ocaml.lua
@@ -1,5 +1,7 @@
 require("notomo.lsp.mapping").setup()
 require("notomo.lsp.autocmd").setup()
+local use_dune_top = vim.fs.find("dune-project", { upward = true, type = "file" })[1] ~= nil
 vim.b.cmdhndlr_runner_opts = {
   use_in_repl = true,
+  use_dune_top = use_dune_top,
 }
