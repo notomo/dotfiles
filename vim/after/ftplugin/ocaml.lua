61ddf071cf240973a1076d2352dc3c9e1021fa4f
notomo <notomo.motono@gmail.com>
2023-01-26 10:29:46 +0900

    Workaround ocaml indentkey

--- vim/after/ftplugin/ocaml.lua
+++ vim/after/ftplugin/ocaml.lua
@@ -1,5 +1,8 @@
 require("notomo.lsp.mapping").setup()
 require("notomo.lsp.autocmd").setup()
+vim.schedule(function()
+  vim.opt_local.indentkeys = ""
+end)
 local use_dune_top = vim.fs.find("dune-project", { upward = true, type = "file" })[1] ~= nil
 vim.b.cmdhndlr_runner_opts = {
   use_in_repl = true,
