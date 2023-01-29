c599be220e5c13af0ea33bde870cc8916de77b17
notomo <notomo.motono@gmail.com>
2023-01-29 09:49:21 +0900

    Use LspRestart

--- vim/lua/notomo/lsp/handler.lua
+++ vim/lua/notomo/lsp/handler.lua
@@ -54,7 +54,7 @@ vim.keymap.set("n", "[lc]D", [[<Cmd>lua vim.lsp.buf.type_definition()<CR>]], { s
 vim.keymap.set("n", "[lc]K", [[<Cmd>lua vim.lsp.buf.signature_help()<CR>]], { silent = true })
 vim.keymap.set("n", "[lc]s", function()
   vim.diagnostic.hide()
-  vim.lsp.stop_client(vim.lsp.get_active_clients(), false)
+  vim.cmd.LspRestart()
 end, { silent = true })
 vim.keymap.set("n", "[exec]gn", [[<Cmd>lua vim.lsp.buf.rename()<CR>]], { silent = true })
 vim.keymap.set("n", "[exec]gf", [[<Cmd>lua vim.lsp.buf.format({async = true})<CR>]])
