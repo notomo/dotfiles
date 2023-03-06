f70c71a100bd62c9ae72d4448365eec7d720e629
notomo <notomo.motono@gmail.com>
2023-03-06 10:06:34 +0900

    Try devcontainer

---
+++ setup/devcontainer.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+gh repo clone notomo/dotfiles ~/dotfiles
+cd ~/dotfiles/ansible
+ANSIBLE_CONFIG=$(pwd) ansible-playbook playbooks/ubuntu.yml -e ansible_python_interpreter=`which python` --tags=workspace --tags=bash --tags=dotfiles --tags=neovim
