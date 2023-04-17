d4085784154d9fb71049b8bda45b0974cbbcae37
notomo <notomo.motono@gmail.com>
2023-04-17 09:58:59 +0900

    Revert format autocmd
    notomo/dotfiles@916d659d

--- vim/lua/notomo/lsp/autocmd.lua
+++ vim/lua/notomo/lsp/autocmd.lua
@@ -1,12 +1,23 @@
 local M = {}
 
-function M.format(bufnr)
-  -- don't specify buffer to reuse client
-  local client = vim.lsp.get_active_clients({ name = "null-ls" })[1]
-  if not client then
-    return require("misclib.message").warn("no null-ls client")
+function M._enable(client, bufnr)
+  local path = vim.api.nvim_buf_get_name(bufnr)
+
+  local root_dir = client.config.root_dir
+  if not vim.endswith(root_dir, "/") then
+    root_dir = root_dir .. "/"
   end
 
+  if vim.startswith(path, root_dir) then
+    return false
+  end
+
+  vim.lsp.stop_client(client.id)
+  require("null-ls").enable({ method = require("null-ls").methods.FORMATTING })
+  return true
+end
+
+function M._format(client, bufnr)
   local params = vim.lsp.util.make_formatting_params()
   local method = "textDocument/formatting"
   client.request(method, params, function(...)
@@ -16,6 +27,34 @@ function M.format(bufnr)
       return vim.cmd.update({ mods = { silent = true, noautocmd = true } })
     end)
   end, bufnr)
+end
+
+function M.format(bufnr)
+  -- don't specify buffer to reuse client
+  local client = vim.lsp.get_active_clients({ name = "null-ls" })[1]
+  if not client then
+    return require("misclib.message").warn("no null-ls client")
+  end
+
+  local reloading = M._enable(client, bufnr)
+  if not reloading then
+    return M._format(client, bufnr)
+  end
+
+  local group = vim.api.nvim_create_augroup("notomo_lsp_format_async", { clear = false })
+  vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+  vim.api.nvim_create_autocmd({ "LspAttach" }, {
+    group = group,
+    buffer = bufnr,
+    callback = function()
+      local c = vim.lsp.get_active_clients({ name = "null-ls" })[1]
+      if not c then
+        return require("misclib.message").warn("no null-ls client")
+      end
+      M._format(c, bufnr)
+      return true
+    end,
+  })
 end
 
 function M.setup()
