23448861f2db6051b6b8c85babb7c9001382f3ee
notomo <notomo.motono@gmail.com>
2024-02-18 20:35:05 +0900

    Fix [keyword]n mapping

--- vim/lua/notomo/plugin/thetto/mapping.lua
+++ vim/lua/notomo/plugin/thetto/mapping.lua
@@ -476,6 +476,11 @@ vim.keymap.set("n", "[keyword]n", function()
         insert = false,
       },
     },
+    modify_pipeline = require("thetto.util.pipeline").prepend({
+      require("thetto.util.filter").item(function(item)
+        return item.path == path
+      end),
+    }),
   }))
 end)
 
