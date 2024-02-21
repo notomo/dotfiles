52363872571509af22ba01d3b06b64819a0fcb82
notomo <notomo.motono@gmail.com>
2024-02-21 20:51:51 +0900

    Workaround to use wslview

--- vim/lua/notomo/lib/browser.lua
+++ vim/lua/notomo/lib/browser.lua
@@ -5,7 +5,16 @@ function M.open(url)
     require("misclib.message").warn("invalid url: " .. url)
     return
   end
-  vim.ui.open(url)
+  if vim.fn.has("wsl") ~= 1 then
+    vim.ui.open(url)
+    return
+  end
+
+  local cmd = { "wslview", url }
+  local result = vim.system(cmd, { text = true, detach = true }):wait()
+  if result.code ~= 0 then
+    require("misclib.message").warn(("failed browser.open: exit_code=%d"):format(result.code))
+  end
 end
 
 return M
