1f3caaf5dd0f8eda929b31d74ce1994c3c7e7f1f
notomo <notomo.motono@gmail.com>
2024-08-17 21:25:48 +0900

    Adjust thetto completion source

--- vim/lua/thetto/handler/source/neosnippet.lua
+++ vim/lua/thetto/handler/source/neosnippet.lua
@@ -1,6 +1,10 @@
 local M = {}
 
-function M.collect()
+function M.collect(source_ctx)
+  if source_ctx.cursor_word.str == "" then
+    return {}
+  end
+
   return vim
     .iter(vim.fn["neosnippet#helpers#get_completion_snippets"]())
     :map(function(snippet)
