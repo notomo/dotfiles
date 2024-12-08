bdedf2939893b21061e871fef07368baa2bb283d
notomo <notomo.motono@gmail.com>
2024-12-08 14:17:21 +0900

    Fix signature help

--- vim/lua/notomo/lsp/signature_help.lua
+++ vim/lua/notomo/lsp/signature_help.lua
@@ -7,6 +7,14 @@ function M.setup()
     buffer = bufnr,
     group = group,
     callback = function()
+      local client = vim.lsp.get_clients({
+        bufnr = bufnr,
+        method = vim.lsp.protocol.Methods.textDocument_signatureHelp,
+      })[1]
+      if not client then
+        return
+      end
+
       vim.lsp.buf.signature_help({
         focusable = false,
         silent = true,
