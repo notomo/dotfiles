b4ea548b2c70aa1ff4b52a69a7e326323ccc6982
notomo <notomo.motono@gmail.com>
2025-01-12 20:25:53 +0900

    Fix plugin manager initialize

--- vim/lua/notomo/plugin/_manager.lua
+++ vim/lua/notomo/plugin/_manager.lua
@@ -4,7 +4,7 @@ vim.opt.packpath:prepend(pack_dir)
 local manager_dir = vim.fs.joinpath(pack_dir, "pack/mypack/opt/optpack.nvim")
 local initializing = vim.fn.isdirectory(manager_dir) ~= 1
 if initializing then
-  vim.cmd["!"]({ args = { "git", "clone", "https://github.com/notomo/optpack.nvim", manager_dir } })
+  vim.system({ "git", "clone", "https://github.com/notomo/optpack.nvim", manager_dir }):wait()
 end
 
 require("notomo.plugin._list")
