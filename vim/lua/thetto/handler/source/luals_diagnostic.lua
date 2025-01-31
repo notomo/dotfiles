2220b4bee48a272ee3b037802092da807688ecb2
notomo <notomo.motono@gmail.com>
2025-01-31 21:16:08 +0900

    Add make check_all

--- vim/lua/thetto/handler/source/luals_diagnostic.lua
+++ vim/lua/thetto/handler/source/luals_diagnostic.lua
@@ -1,19 +1,67 @@
+local pathlib = require("thetto.lib.path")
+
 local M = {}
 
 function M.collect(source_ctx)
-  local cmd = { "nvim", "--headless", "+lua require('notomo.lib.startup').diagnostic()" }
-  return require("thetto.util.job").start(cmd, source_ctx, function(output)
-    local x = vim.json.decode(output)
-    local relative_path = require("thetto.lib.path").to_relative(x.path, source_ctx.cwd)
-    return {
-      value = relative_path,
-      desc = ("%s:%d %s"):format(relative_path, x.row + 1, x.message:gsub("\n", " ")),
-      path = x.path,
-      row = x.row + 1,
-    }
-  end)
+  local output_path = vim.fn.tempname()
+  local cmd = {
+    "lua-language-server",
+    "--check=lua",
+    "--check_out_path",
+    output_path,
+    "--configpath",
+    vim.fs.joinpath(require("optpack").get("workflow").directory, ".luarc.json"),
+  }
+  return require("thetto.util.job")
+    .promise(cmd, {
+      cwd = source_ctx.cwd,
+      on_exit = function() end,
+    })
+    :next(function()
+      local f = assert(io.open(output_path))
+      local content = f:read("*a")
+      f:close()
+
+      local items_map = vim
+        .iter(vim.json.decode(content))
+        :map(function(uri, diagnostics)
+          return vim
+            .iter(diagnostics)
+            :map(function(diagnostic)
+              local path = vim.uri_to_fname(uri)
+              local relative_path = pathlib.to_relative(path, source_ctx.cwd)
+              local message = diagnostic.message:gsub("\n", " ")
+              local desc = ("%s %s [%s:%s]"):format(relative_path, message, diagnostic.source, diagnostic.code)
+              return {
+                value = message,
+                desc = desc,
+                row = diagnostic.range.start.line + 1,
+                column = diagnostic.range.start.character,
+                end_column = diagnostic.range["end"].character,
+                path = path,
+                severity = diagnostic.severity,
+                column_offsets = {
+                  path = 0,
+                  value = #relative_path + 1,
+                },
+              }
+            end)
+            :totable()
+        end)
+        :totable()
+      return vim.iter(vim.tbl_values(items_map)):flatten():totable()
+    end)
 end
 
 M.kind_name = "file"
+
+M.cwd = require("thetto.util.cwd").upward({ "lua", ".git" })
+
+M.highlight = require("thetto.util.highlight").columns({
+  {
+    group = "Comment",
+    end_key = "value",
+  },
+})
 
 return M
