54cad742bf977edaf85beee5ff338091744a8e8f
notomo <notomo.motono@gmail.com>
2025-03-15 20:18:36 +0900

    Small refactor winbar

--- vim/lua/notomo/lsp/navigation.lua
+++ vim/lua/notomo/lsp/navigation.lua
@@ -14,6 +14,50 @@ function M.attach(client, bufnr)
   end
 end
 
+local setup_stlparts = function()
+  local join_by = require("misclib.collection.list").join_by
+  local stlparts = require("stlparts")
+  local C = stlparts.component
+  local Builder = C.builder
+  local Highlight = C.highlight
+  local DefaultHighlight = C.default_highlight
+
+  local no_scope = Highlight("WinbarNavic", "(no scope)")
+  local separator = Highlight("WinbarNavicSeparator", " > ")
+
+  local ignore_types = {
+    "Variable",
+  }
+  stlparts.set("navic", {
+    DefaultHighlight("Normal", {
+      Highlight("WinbarNavicSeparator", " > "),
+      Builder(function(ctx)
+        local bufnr = vim.api.nvim_win_get_buf(ctx.window_id)
+        local navic = require("nvim-navic")
+        if not navic.is_available(bufnr) then
+          return ""
+        end
+        local data = navic.get_data(bufnr)
+        if not data or #data == 0 then
+          return no_scope
+        end
+        local names = vim
+          .iter(data)
+          :filter(function(d)
+            return not vim.tbl_contains(ignore_types, d.type)
+          end)
+          :map(function(d)
+            return Highlight("WinbarNavic", d.name)
+          end)
+          :totable()
+        return join_by(names, separator)
+      end),
+      " ",
+    }),
+  })
+end
+setup_stlparts()
+
 function M.setup()
   local filetype = vim.bo.filetype
   if not M.filetypes[filetype] then
