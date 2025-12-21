1eeb3fa602ce85ea0f8b5663334871655bb52183
notomo <notomo.motono@gmail.com>
2025-12-21 20:27:54 +0900

    Remove ssh key from local

--- setup/ubuntu/adjust.sh
+++ setup/ubuntu/adjust.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gh auth login --git-protocol=ssh --hostname=github.com --scopes=project
+gh auth login --with-token < /tmp/token.txt
 [ ! -d ~/workspace/todo ] && gh repo clone notomo/todo ~/workspace/todo
 
 [ ! -f "${DOTFILES}"/vim/lua/notomo/local/local.lua ] && cp "${DOTFILES}"/vim/lua/notomo/local/local.lua.sample "${DOTFILES}"/vim/lua/notomo/local/local.lua
