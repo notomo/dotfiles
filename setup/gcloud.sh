c4790302fa2c3ac083ffc49065aca3d69e2c2f47
notomo <notomo.motono@gmail.com>
2025-12-26 10:04:55 +0900

    Remove gcloud task

---
+++ setup/gcloud.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -eu
+
+APP_DIR="$HOME/app"
+GCLOUD_DIR="$APP_DIR/gcloud"
+GCLOUD_TARBALL="$APP_DIR/gcloud.tar.gz"
+
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+case "$OS" in
+  linux)
+    GCLOUD_URL="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz"
+    ;;
+  darwin)
+    GCLOUD_URL="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-darwin-x86_64.tar.gz"
+    ;;
+  *)
+    echo "Unsupported OS: $OS"
+    exit 1
+    ;;
+esac
+
+mkdir -p "$GCLOUD_DIR"
+if [ ! -f "$GCLOUD_TARBALL" ]; then
+  curl -L -o "$GCLOUD_TARBALL" "$GCLOUD_URL"
+fi
+tar -xzf "$GCLOUD_TARBALL" -C "$GCLOUD_DIR"
+
+ZSHRC="$HOME/.zshrc"
+MARKER_START="# BEGIN ANSIBLE MANAGED BLOCK gcloud role"
+MARKER_END="# END ANSIBLE MANAGED BLOCK gcloud role"
+GCLOUD_PATH_SCRIPT="$HOME/app/gcloud/google-cloud-sdk/path.zsh.inc"
+if ! grep -q "$MARKER_START" "$ZSHRC" 2>/dev/null; then
+  echo "Adding gcloud configuration to .zshrc..."
+  cat >> "$ZSHRC" << EOF
+$MARKER_START
+if [ -f '$GCLOUD_PATH_SCRIPT' ]; then . '$GCLOUD_PATH_SCRIPT'; fi
+$MARKER_END
+EOF
+fi
+
+cd ~/app/gcloud/google-cloud-sdk
+./install.sh
