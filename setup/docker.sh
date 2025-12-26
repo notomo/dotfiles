80b6a80c4789aff7cfc04599a2093741b080c4c3
notomo <notomo.motono@gmail.com>
2025-12-26 14:36:18 +0900

    Remove docker task, ansible

---
+++ setup/docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+sudo install -m 0755 -d /etc/apt/keyrings
+if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+fi
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $VERSION_CODENAME stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker "$USER"
+
+if command -v cgroupfs-mount &> /dev/null; then
+  sudo cgroupfs-mount
+fi
+sudo service docker start || true
