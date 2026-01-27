eb35a586a7e4c69cef9ac5807ffcf38aac7d0459
notomo <notomo.motono@gmail.com>
2026-01-27 21:07:25 +0900

    Fix neovim update

--- tool/update_macos_neovim.sh
+++ tool/update_macos_neovim.sh
@@ -5,7 +5,7 @@ install_dir=$(mise where github:neovim/neovim)
 if [ -d "$install_dir" ]; then
   backup_dir="${install_dir}.backup.$(date +%Y%m%d_%H%M%S)"
   echo "Backing up $install_dir to $backup_dir"
-  cp "$install_dir" "$backup_dir"
+  cp -rf "$install_dir" "$backup_dir"
 fi
 
 mise install -fv github:neovim/neovim@nightly
